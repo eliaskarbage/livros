51896bfc45c3b7341870872dce50cee3f427ff9e
Seu nome aqui <seuemail@email.com>
2025-08-01 12:47:52 -0300

    estrutura inicial

---
+++ sistema-livros/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import 'primereact/resources/themes/lara-light-blue/theme.css'; 
+import 'primereact/resources/primereact.min.css';               
+import 'primeicons/primeicons.css';                             
+import 'primeflex/primeflex.css';   
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       <h1>Projeto aluguel de livros!</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
